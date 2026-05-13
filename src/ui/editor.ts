c38855e25139aeec1be1b569ae297fe15e99f0de
altwine <180307749+altwine@users.noreply.github.com>
2026-05-14 02:22:08 +0300

    refactor: color revamp

--- src/ui/editor.ts
+++ src/ui/editor.ts
@@ -1,14 +1,60 @@
 import { EXAMPLE_CODE_SNIPPETS } from "../core/examples.ts";
 import { EDITOR, STATUS_BAR_CURSOR_POSITION } from "./elements.ts";
-
 import { EditorState } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
-import { basicSetup } from "codemirror";
+import {
+	EditorView,
+	lineNumbers,
+	highlightActiveLineGutter,
+	highlightSpecialChars,
+	drawSelection,
+	dropCursor,
+	rectangularSelection,
+	crosshairCursor,
+	highlightActiveLine,
+	keymap,
+} from "@codemirror/view";
+import {
+	foldGutter,
+	indentOnInput,
+	syntaxHighlighting,
+	defaultHighlightStyle,
+	bracketMatching,
+	foldKeymap,
+} from "@codemirror/language";
+import { history, defaultKeymap, historyKeymap } from "@codemirror/commands";
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
+import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete";
+import { lintKeymap } from "@codemirror/lint";
 
 const state = EditorState.create({
 	doc: EXAMPLE_CODE_SNIPPETS["рыбалка.карп"],
 	extensions: [
-		basicSetup,
+		lineNumbers(),
+		highlightActiveLineGutter(),
+		highlightSpecialChars(),
+		history(),
+		foldGutter(),
+		drawSelection(),
+		dropCursor(),
+		EditorState.allowMultipleSelections.of(true),
+		indentOnInput(),
+		syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+		bracketMatching(),
+		closeBrackets(),
+		autocompletion(),
+		rectangularSelection(),
+		crosshairCursor(),
+		highlightActiveLine(),
+		highlightSelectionMatches(),
+		keymap.of([
+			...closeBracketsKeymap,
+			...defaultKeymap,
+			...searchKeymap,
+			...historyKeymap,
+			...foldKeymap,
+			...completionKeymap,
+			...lintKeymap,
+		]),
 		EditorView.updateListener.of((update) => {
 			if (update.selectionSet || update.docChanged) {
 				const { head } = update.view.state.selection.main;
@@ -16,6 +62,24 @@ const state = EditorState.create({
 				const col = head - line.from + 1;
 				STATUS_BAR_CURSOR_POSITION.textContent = `${line.number}:${col}`;
 			}
+		}),
+		EditorView.theme({
+			"&": {
+				backgroundColor: "#0d1016",
+				color: "#bfbdb6",
+			},
+			".cm-activeLine": {
+				backgroundColor: "#1a1d23",
+			},
+			".cm-gutters": {
+				backgroundColor: "#0d1016",
+				color: "#4b4c4e",
+				border: "none",
+			},
+			".cm-activeLineGutter": {
+				backgroundColor: "#1a1d23",
+				color: "#cbcccd",
+			},
 		}),
 	],
 });
